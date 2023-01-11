47e8e155ba4d218421081c5f146b11c881ded5c8
Sudarsh Suresh Mallaya <sudarsh.sureshmallaya@nxp.com>
2023-01-11 10:24:45 +0530

    added third number sum

--- SSM.py
+++ SSM.py
@@ -3,9 +3,10 @@ print("Hello world successful");
 # Store input numbers
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
 
-# Add two numbers
-sum = float(num1) + float(num2)
+# Add third numbers
+sum = float(num1) + float(num2) + float(num3)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, num3, sum))
