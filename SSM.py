fe63c2f18e2b39340dd427d6aa6fa6266b4e79f5
Sudarsh <smallaya@gmail.com>
2023-01-11 06:55:03 +0530

    add 2 nos

--- SSM.py
+++ SSM.py
@@ -1,3 +1,11 @@
-pwd
-date
-print("HEllo world successful");
+print("Hello world successful");
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
